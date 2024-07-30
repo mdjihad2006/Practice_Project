89e57b19b997579add4f16cf364111a427f4dc32
mdjihad2006 <md.jihad.info2006@gmail.com>
2024-07-30 13:08:41 +0600

    Container_Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -11,50 +11,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Daraz'),
-          backgroundColor: Colors.deepOrange,
-        ),
-        body: ListView.separated(
-          itemCount: 20,
-          itemBuilder: (context, index){
-            return ListTile(
-              tileColor: Colors.tealAccent,
-              title: Image.asset('assets/images/T-Shirt pic.jpg',
-                width: 200,
-                height: 200,
-                fit: BoxFit.cover,
-              ),
-              subtitle: Text('A new T-shirt arrived'),
-              trailing: Column(
-                children: [
-                  Icon(Icons.favorite, color:  Colors.pink,),
-                  Text('Fav')
-                ],
-              ),
-              onTap: (){
-                print('$index item taped');
-              },
-            );
-          },
-          separatorBuilder: (context, index) {
-            return Divider(
-              color: Colors.deepOrange,
-            );
-          },
-        ),
-        // body: GridView.builder(
-        //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, crossAxisSpacing: 8, mainAxisSpacing: 8),
-        //   itemCount: 20,
-        //   itemBuilder: (context, index) {
-        //     return Image.asset('assets/images/T-Shirt pic.jpg',
-        //               width: 200,
-        //               height: 200,
-        //             );
-        //   },
-        // ),
-      ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Daraz'),
+              backgroundColor: Colors.deepOrange,
+            ),
+            body:Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        width: 100,
+                        height: 100,
+                        alignment: Alignment.bottomRight,
+                        padding: EdgeInsets.all(15),
+                        margin: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            border: Border.all(color: Colors.pink, width: 5),
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(16),
+                                bottomRight: Radius.circular(16)
+                            ),
+                            // shape: BoxShape.circle
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/T-Shirt pic.jpg'),
+                                fit: BoxFit.cover,
+                                opacity: 0.6
+                            ),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.deepPurple.withOpacity(0.6),
+                                spreadRadius: 3,
+                                blurRadius: 10,
+                                offset: Offset(0,5),
+                              ),
+                            ]
+                        ),
+                        child: Icon(Icons.add_a_photo, color: Colors.red)
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          showAboutDialog(context: context);
+                        },
+                      child: Text('Click')
+                    )
+                  ],
+                )
+            )
+        )
     );
   }
 }
