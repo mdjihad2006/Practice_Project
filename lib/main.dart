ea05f11d1cf7666fe06a7876b00aeaa6d1512fe8
mdjihad2006 <md.jihad.info2006@gmail.com>
2024-08-20 19:14:35 +0600

    Responsive design

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),);
 }
 
 class MyApp extends StatelessWidget {
@@ -10,37 +14,169 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-        home: MyHomePage(),
-    );
-  }
-}
-class MyHomePage extends StatelessWidget{
-  const MyHomePage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Profile'),
-        leading: Icon(CupertinoIcons.back),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('This is Cupertino design system',
-              style: TextStyle(color: Colors.deepPurple),
-            ),
-            SizedBox(height: 20,),
-            Icon(CupertinoIcons.airplane, size: 50, color: Colors.deepOrangeAccent,),
-            SizedBox(height: 20,),
-            CupertinoButton.filled(child: Text('Button'), onPressed: (){
-
-            })
-          ],
+    return  MaterialApp(
+      home: const HomeActivity(),
+      title: 'Theming',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.green,
+          centerTitle: true,
         ),
       ),
     );
   }
+}
+class HomeActivity extends StatefulWidget{
+  const HomeActivity({super.key});
 
+  @override
+  State<StatefulWidget> createState() => HomeState();
+
+}
+class HomeState extends State<HomeActivity>{
+  @override
+  Widget build(BuildContext context) {
+
+    // MediaQueryData mediaQuery = MediaQuery.of(context);
+    //
+    // if(mediaQuery.size.width < 400){
+    //   print('This is Mobile phone');
+    // }
+    // else if(mediaQuery.size.width >400 && mediaQuery.size.width < 1080){
+    //   print('This is a tablet');
+    // }
+    // else{print('This is Laptop');}
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Theming'),
+      ),
+      // body: Column(
+      //   children: [
+      //     Flexible(
+      //       flex: 1,
+      //         child: Container(
+      //           height: 300,
+      //           width: double.infinity,
+      //           color: Colors.deepOrange,
+      //         )
+      //     ),
+      //     Flexible(
+      //         flex: 1,
+      //         child: Container(
+      //           height: 300,
+      //           width: double.infinity,
+      //           color: Colors.pink,
+      //         )
+      //     ),
+      //     Flexible(
+      //         flex: 1,
+      //         child: Container(
+      //           height: 300,
+      //           width: double.infinity,
+      //           color: Colors.deepPurple,
+      //         )
+      //     ),
+      //     Flexible(
+      //         flex: 1,
+      //         child: Container(
+      //           height: 300,
+      //           width: double.infinity,
+      //           color: Colors.blueAccent,
+      //         )
+      //     )
+      //   ],
+      // )
+
+
+      // body: AspectRatio(
+      //     aspectRatio: 16/8,
+      //   child: Center(
+      //     child: Container(
+      //       height: 200,
+      //       width: 200,
+      //       color: Colors.deepOrange,
+      //       alignment: Alignment.center,
+      //     ),
+      //   ),
+      // ),
+
+
+      // body: Wrap(
+      //   alignment: WrapAlignment.center,
+      //   crossAxisAlignment: WrapCrossAlignment.center,
+      //   spacing: 2,
+      //   children: [
+      //     Text('Hello mr.Jihad,glad to see you'),
+      //     Text('Hello mr.Jihad'),
+      //     Text('Hello mr.Jihad,glad to see you'),
+      //     Text('Hello mr.Jihad'),
+      //     Text('Hello mr.Jihad'),
+      //     Text('Hello mr.Jihad'),
+      //   ],
+      // ),
+      // body: OrientationBuilder(
+      //   builder:(context, Orientation orientation){
+      //     if(orientation == Orientation.portrait){
+      //       return  Center(
+      //           child: Container(
+      //             height: 200,
+      //             width: 200,
+      //             color: Colors.pink,
+      //           )
+      //       );
+      //     }
+      //     else{
+      //       return  Center(
+      //           child: Container(
+      //             height: 200,
+      //             width: 200,
+      //             color: Colors.deepPurple,
+      //           )
+      //       );
+      //     }
+      //   }
+      // ),,
+      // body: LayoutBuilder(
+      //   builder: (context, BoxConstraints constraints){
+      //     if(constraints.maxWidth < 640){
+      //       return Center(
+      //         child: Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.pink,
+      //           child: Center(
+      //             child: Text('This is a phone')
+      //           ),
+      //         ),
+      //       );
+      //     }
+      //     else if(constraints.maxWidth > 640 && constraints.maxWidth < 1008){
+      //       return Center(
+      //         child: Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.blueAccent,
+      //           child: Center(
+      //             child: Text('This is a Tablet')
+      //           ),
+      //         ),
+      //       );
+      //     }
+      //     else{
+      //       return Center(
+      //         child: Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.green,
+      //           child: Center(
+      //             child: Text('This is a Laptop')
+      //           ),
+      //         ),
+      //       );
+      //     }
+      //   }
+      // ),
+    );
+  }
 }
